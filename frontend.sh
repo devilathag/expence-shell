8e418e9685a5e61fefa3a6fcc90b3e521a60b5d6
devilatha <latha.korm@gmail.com>
2023-09-10 16:17:51 +0530

    expence shell script

--- frontend.sh
+++ frontend.sh
@@ -30,7 +30,23 @@ fi
 
 cd /usr/share/nginx/html
 
-download_and_extract
+echo Download $component Code
+  curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
+
+echo Extracting $component Code
+  unzip /tmp/$component.zip >>$log_file
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
 
 echo Starting Nginx Service
 systemctl enable nginx &&>>$log_file
