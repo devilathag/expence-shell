153b23f14518af2230370452ff4baf1cf0bfb8f7
devilatha <latha.korm@gmail.com>
2023-09-10 16:55:42 +0530

    expence shell script

--- mysql.sh
+++ mysql.sh
@@ -1,24 +1,49 @@
-source common.sh
+log_file=/tmp/expense.log
 
 echo Disable MySQL 8 Version
 dnf module disable mysql -y &>>$log_file
-stat_check
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
 
 
 echo Copy MySQL Repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-stat_check
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
 
 echo Install MySQL Server
 dnf install mysql-community-server -y &>>$log_file
-stat_check
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
 
 echo Start MySQL Service
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
-stat_check
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
 
 echo Setup root Password
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-stat_check
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
 
