a08ee8930bb83c767cf8eda6265b20cb6a7c27c7
devilatha <latha.korm@gmail.com>
2023-09-10 16:05:50 +0530

    expence shell script

--- frontend.sh
+++ frontend.sh
@@ -3,15 +3,30 @@ component=frontend
 
 echo Installing Nginx
 dnf install nginx -y &>>$log_file
-stat_check
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
 
 echo Placing Expense Config File in Nginx
 cp expense.conf /etc/nginx/default.d/expense.conf &&>>$log_file
-stat_check
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
 
 echo Removing Old Nginx content
 rm -rf /usr/share/nginx/html/* &&>>$log_file
-stat_check
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
 
 cd /usr/share/nginx/html
 
@@ -20,4 +35,9 @@ download_and_extract
 echo Starting Nginx Service
 systemctl enable nginx &&>>$log_file
 systemctl restart nginx &&>>$log_file
-stat_check
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+fi
