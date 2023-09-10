108ac690d3190cdc376e9d14c16ad7facc2df0d8
devilatha <latha.korm@gmail.com>
2023-09-10 13:19:53 +0530

    expence shell script

--- mysql.sh
+++ mysql.sh
@@ -2,11 +2,12 @@ source common.sh
 
 echo Disable MySQL 8 Version
 dnf module disable mysql -y &>>$log_file
+stat_check
 
 
 echo Copy MySQL Repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-
+stat_check
 
 echo Install MySQL Server
 dnf install mysql-community-server -y &>>$log_file
@@ -18,7 +19,6 @@ systemctl start mysqld &>>$log_file
 stat_check
 
 echo Setup root Password
-mysql_root_password=$1
-mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
 stat_check
 
